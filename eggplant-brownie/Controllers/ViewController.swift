3e8a910cca22c6152b56e62278821b30b8cd0548
MarioFelesDosSantosJunior <mario.feles@useblu.com.br>
2021-06-06 18:34:40 -0300

    implements select itens

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -11,12 +11,54 @@ protocol AdicionaRefeicaoDelegate {
     func add(_ refeicao: Refeicao)
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: - Atributos
     
     var delegate: AdicionaRefeicaoDelegate?
+    var itens: Array<Item> = [
+        Item(nome: "Molho de tomate", calorias: 10),
+        Item(nome: "Queijo", calorias: 8),
+        Item(nome: "Molho de pimenta", calorias: 1),
+        Item(nome: "Manjericão", calorias: 3)
+    ]
+    var itensSelecionados: Array<Item> = []
+    
+    // MARK: - IBOutlet
     
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet var felicidadeTextField: UITextField?
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = itens[indexPath.row].nome
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+        
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        if(cell.accessoryType == .none){
+            cell.accessoryType = .checkmark
+            addItem(indexPath)
+        }else {
+            cell.accessoryType = .none
+        }
+    }
+    
+    fileprivate func addItem(_ indexPath: IndexPath) {
+        let item = itens[indexPath.row]
+        itensSelecionados.append(item)
+    }
+    
+    // MARK: - IBAction
     
     @IBAction func adiciona() {
         
@@ -28,10 +70,9 @@ class ViewController: UIViewController {
             return
         }
         
-        let refeicao = Refeicao(nome: nomeRefeicao, felicidade: felicidade)
+        let refeicao = Refeicao(nome: nomeRefeicao, felicidade: felicidade,itens: itensSelecionados)
         
         delegate?.add(refeicao)
         navigationController?.popViewController(animated: true)
     }
-
 }
